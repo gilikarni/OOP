08c4981f714531e6fa842ad51b384d33c0bd6a66
gilikarni <gilikarni@campus.technion.ac.il>
2017-11-07 19:39:05 +0200

    EX0: Q2: Start to implement
    > Start to impelement main
    > Implement Constructor
    > Start to implement a function to count syllables

--- ex0/Q2/src/Flesch.java
+++ ex0/Q2/src/Flesch.java
@@ -1,7 +1,52 @@
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
 public class Flesch {
 
+    private FileReader fr;
+    private Integer numberOfWords;
+    private Integer numberOfSent;
+    private Integer numberOfSyl;
+
+    static int main(String args[]) throws Exception {
+        Flesch fl = new Flesch(args[0]);
+
+        BufferedReader br = new BufferedReader(fr);
+        String s;
+        while((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+        return 0;
+    }
+
+    private Flesch(String fileName) throws Exception {
+        fr = new FileReader(fileName);
+        numberOfWords = 0;
+        numberOfSent = 0;
+        numberOfSyl = 0;
+    }
+
+    private void finish() throws IOException {
+        fr.close();
+    }
+
+    /**
+     * @modifies
+     * @effects Count number of syllables in a word (s must be a word)
+     * @return Return the number of syllables in the word s
+     */
+    static int countSils(String string) {
+        Integer numOfSyls = 0;
+        char sylls[] = {'a', 'e', 'i', 'o', 'u', 'y'};
+
+        String strArr[] = string.split("^(?!aeiouy)");
+
+        // Count numbet strings in the array and remove illegals
+
+        if (0 == numOfSyls) {
+            return 1;
+        }
+        return numOfSyls;
+    }
 }

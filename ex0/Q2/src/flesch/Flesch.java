127984629a4b15b66000847a5eb0d530a7842627
galshachaf <galshac@gmail.com>
2017-11-11 17:14:56 +0200

    EX0: Q2: Finish the exercise
    > Finish the implementation
    > Add test files
    
    Signed-off-by: galshachaf <galshac@gmail.com>

--- ex0/Q2/src/flesch/Flesch.java
+++ ex0/Q2/src/flesch/Flesch.java
@@ -2,9 +2,10 @@ package flesch;
 
 import java.io.FileReader;
 import java.io.BufferedReader;
-import java.io.IOException;
-import java.util.Arrays;
 
+/**
+ * counts sentences, words, and syllables in a given file.
+ */
 public class Flesch {
 
     private FileReader fr;
@@ -13,51 +14,67 @@ public class Flesch {
     private Integer numberOfSyl;
 
     public static void main(String args[]) throws Exception {
-        Flesch fl = new Flesch(args[0]);
+        String fileName = args[0];
+        FileReader fr = new FileReader(fileName);
+        Integer numberOfWords = 0;
+        Integer numberOfSent = 0;
+        Integer numberOfSyl = 0;
 
-        BufferedReader br = new BufferedReader(fl.fr);
+        BufferedReader br = new BufferedReader(fr);
+        String text_sentances = "";
+        String text_words = "";
         String s;
         while((s = br.readLine()) != null) {
+            text_sentances += s;
+            text_words += s;
+            text_words += ' ';
+        }
+        // count sentences
+        String sentenceArr[] = text_sentances.split("[.,;?!]");
+        for (String sentence : sentenceArr) {
+            if (!sentence.isEmpty()) {
+                numberOfSent += 1;
+            }
+        }
+        // count words
+        String wordArr[] = text_words.split("[()*&^%$#@!_+\\-={}?;:',.\\[\\]\" ]");
+        for (String word : wordArr) {
+            if (!word.isEmpty()) {
+                numberOfWords += 1;
+                numberOfSyl += countSyls(word);
+            }
         }
 
-        countSyls("real");
-    }
+        Double fleschScore = 206.835 - (84.6 * numberOfSyl.floatValue() / numberOfWords.floatValue()) - (1.015 *
+                numberOfWords.floatValue() / numberOfSent.floatValue());
 
-    private Flesch(String fileName) throws Exception {
-        fr = new FileReader(fileName);
-        numberOfWords = 0;
-        numberOfSent = 0;
-        numberOfSyl = 0;
-    }
+        // Go Sinoff, Go Sinoff!
 
-    private void finish() throws IOException {
+        System.out.println("File " + fileName + " contains " + numberOfSent + " sentences, " + numberOfWords +
+                " words, and " + numberOfSyl + " syllables. the Flesch Score of this file is " + fleschScore + ".");
         fr.close();
+
     }
 
     /**
+     * @requires a string containing one word
      * @modifies
      * @effects Count number of syllables in a word (s must be a word)
      * @return Return the number of syllables in the word s
      */
     static int countSyls(String string) {
         Integer numOfSyls = 0;
-        char sylls[] = {'a', 'e', 'i', 'o', 'u', 'y'};
-
-        String strArr[] = string.split("[aeiouy]");
-
-        System.out.println(Arrays.toString(strArr));
-
-        // Count numbet strings in the array and remove illegals
-        boolean bFrist = true;
-        for (String s : strArr) {
-            if (bFrist || (s != null && s.length() > 0)) {
-                numOfSyls++;
-            }
-            bFrist = false;
+        if (string.endsWith("e")) {
+            string = string.substring(0, string.length() - 1);
         }
-
-        if (0 == numOfSyls) {
-            return 1;
+        String sylArr[] = string.split("[^aAeEiIoOuUyY]");
+        for (String s : sylArr) {
+            if (!s.isEmpty()) {
+                numOfSyls += 1;
+            }
+        }
+        if (numOfSyls == 0) {
+            numOfSyls = 1;
         }
         return numOfSyls;
     }

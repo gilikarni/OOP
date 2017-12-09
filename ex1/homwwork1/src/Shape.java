0d448b10abebace27d73a8b84c81f3c985efd3c6
gilikarni <gilikarni@campus.technion.ac.il>
2017-12-09 20:16:00 +0200

    Tumtumim

--- ex1/homwwork1/src/Shape.java
+++ ex1/homwwork1/src/Shape.java
@@ -70,7 +70,7 @@ public abstract class Shape implements Cloneable {
      * @modifies this
      * @effects Resizes this so that its bounding rectangle has the specified
      *          dimension.
-     *          If this cannot be resized to the specified dimension =>
+     *          If this cannot be resized to the specified dimension ={@literal >}
      *          this is not modified, throws ImpossibleSizeException
      *          (the exception suggests an alternative dimension that is
      *           supported by this).

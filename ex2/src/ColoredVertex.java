129c898746005e3e725811d39541577780966472
gilikarni <gilikarni@campus.technion.ac.il>
2017-12-12 17:18:08 +0200

    Create Vertex API

--- ex2/src/ColoredVertex.java
+++ ex2/src/ColoredVertex.java
@@ -7,7 +7,7 @@ import java.util.*;
 /**
  * TODO
  */
-public class Vertex<T, S> {
+public class ColoredVertex<T, S> {
     private T label;
     private Map<S, T> parents;
     private Map<S, T> children;
@@ -20,7 +20,8 @@ public class Vertex<T, S> {
     /**
      * Create a new vertex for the graph..
      */
-    public Vertex() {
+    public ColoredVertex() {
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
@@ -29,6 +30,7 @@ public class Vertex<T, S> {
      * @effects Add a new parent to this vertex
      */
     public void addParent(S edgeLabel, T parentLabel) {
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
@@ -37,6 +39,7 @@ public class Vertex<T, S> {
      * @effects Add a new son to this vertex
      */
     public void addSon(S edgeLabel, T sonLabel) {
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
@@ -46,6 +49,7 @@ public class Vertex<T, S> {
      * @return true if the vertex has a parent with the edge label edgeLabel
      */
     public boolean isParentExist(S edgeLabel) {
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
@@ -55,6 +59,23 @@ public class Vertex<T, S> {
      * @return true if the vertex has a son with the edge label edgeLabel
      */
     public boolean isSonExist(S edgeLabel) {
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return return true if the vertex is black
+     */
+    public boolean isVertexBlack() {
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return return true if the vertex is white
+     */
+    public boolean isVertexWhite() {
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 }

8777d47b70a8d61ff8ab604eef7b594998ffd1a7
gilikarni <gilikarni@campus.technion.ac.il>
2017-12-12 17:17:35 +0200

    Create Vertex API

--- ex2/src/BipartiteGraph.java
+++ ex2/src/BipartiteGraph.java
@@ -7,39 +7,33 @@ import java.util.*;
  * T is the label of a vertex, S is a label of an edge.
  */
 public class BipartiteGraph<T, S>{
-    public enum BipartiteGraphReturnValue {
-        SUCCESS,
-        VERTEX_ALREADY_EXIST,
-        VERTEX_DOESNT_EXIST,
-        EDGE_ALREADY_EXIST,
-        EDGE_DOESNT_EXIST,
-        ILLEGAL_EDGE,
-    }
-
     Map<T, Vertex<T, S>> vertexes;
 
     /**
      * TODO
      */
     public BipartiteGraph() {
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
     /**
      * @modifies this
      * @effects Create a new white vertex with the label vertexLabel.
-     * @return BipartiteGraphReturnValue TODO
+     * @throws IllegalArgumentException if there is already a vertex with this label
      */
-    public BipartiteGraphReturnValue addWhiteVertex(T vertexLabel) {
+    public void addWhiteVertex(T vertexLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
     /**
      * @modifies this
      * @effects Create a new black vertex with the label vertexLabel.
-     * @return BipartiteGraphReturnValue TODO
+     * @throws IllegalArgumentException if there is already a vertex with this label
      */
-    public BipartiteGraphReturnValue addBlackVertex(T vertexLabel) {
+    public void addBlackVertex(T vertexLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
@@ -47,19 +41,64 @@ public class BipartiteGraph<T, S>{
      * @modifies this
      * @effects add a new edge to the graph from sourceVertex to targetVertex. The new edge label must be unique for
      * both the vertexes. sourceVertex and targetVertex must be from different sets.
-     * @return BipartiteGraphReturnValue TODO
+     * @throws IllegalArgumentException if the
      */
-    public BipartiteGraphReturnValue addEdge(T sourceVertex, T targetVertex, S edgeLabel) {
+    public void addEdge(T sourceVertex, T targetVertex, S edgeLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
         throw new NotImplementedException();
     }
 
     /**
-     * @return a space-separated list of the names of all the black vertexes
-     * 		   in the BipartiteGraph
+     * @return a list of all the black vertexes labels in the graph
      */
-    public String listBlackNodes() {
+    public List<T> getListOfBlackVertexes() {
         //TODO: Implement this method
+        throw new NotImplementedException();
+    }
 
+    /**
+     * @return a list of all the white vertexes labels in the graph
+     */
+    public List<T> getListOfWhiteVertexes() {
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
 
+    /**
+     * @return a list of the parents labels of the vertex with the label vertexLabel
+     * @throws IllegalArgumentException if there is no vertex with the label vertexLabel
+     */
+    public List<T> getListOfVertexParents(T vertexLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return a list of the children labels of the vertex with the label vertexLabel
+     * @throws IllegalArgumentException if there is no vertex with the label vertexLabel
+     */
+    public List<T> getListOfVertexChildren(T vertexLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return The label of the child vertex
+     * @throws IllegalArgumentException if the vertex doesn't exist or the vertex doesn't have a
+     * child with the label edgeLabel.
+     */
+    public T getChildByEdgeLabel(T parentLabel, S edgeLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
+        throw new NotImplementedException();
+    }
+
+    /**
+     * @return The label of the parent vertex
+     * @throws IllegalArgumentException if the vertex doesn't exist or the vertex doesn't have a
+     * parent with the label edgeLabel.
+     */
+    public T getParentByEdgeLabel(T childLabel, S edgeLabel) throws IllegalArgumentException{
+        //TODO: Implement this method
+        throw new NotImplementedException();
     }
 }

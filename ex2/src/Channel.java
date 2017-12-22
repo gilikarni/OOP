4dd92d342921ebb76b2d853ae72f3df47adcb077
gilikarni <gilikarni@campus.technion.ac.il>
2017-12-22 18:08:53 +0200

    Channel

--- ex2/src/Channel.java
+++ ex2/src/Channel.java
@@ -1,8 +1,11 @@
-public class Channel<T, S> extends Pipe<T, S> {
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Channel extends Pipe<String, Transaction> {
     private int maxPayments = 0;
 
-    public Channel(T label, int maxPayments) {
-        super(label);
+    public Channel(String name, int maxPayments) {
+        super(name);
         this.maxPayments = maxPayments;
 
         checkRep();
@@ -10,6 +13,45 @@ public class Channel<T, S> extends Pipe<T, S> {
 
     @Override
     public void simulate(BipartiteGraph graph) {
+        ArrayList<ColoredVertex<String>> children = getChildrenList();
+        assert children.size() <= 1 : "Channel has " + children.size() + "children";
 
+        /* Nowhere to pass the transaction to */
+        if (children.size() == 0) {
+            return;
+        }
+
+        Participant target = (Participant)children.get(0);
+
+        Iterator<Transaction> iterator = getWorkObjectsIterator();
+        while (iterator.hasNext()) {
+            Transaction transaction = iterator.next();
+            target.addTransaction(transaction);
+        }
+
+        clearWorkObjects();
+    }
+
+    /**
+     * @modifies this
+     * @effects adds a new transaction to the pipe if the value of the transaction is not higher than the current
+     * maximum allowed value for the channel.
+     * @return true if the transaction was added successfully, false otherwise
+     */
+    @Override
+    public boolean addWorkObject(Transaction workobject) {
+        boolean bSuccess = false;
+        if (workobject.getValue() > maxPayments) {
+            return bSuccess;
+        }
+
+        bSuccess = super.addWorkObject(workobject);
+
+        if (bSuccess) {
+            maxPayments -= workobject.getValue();
+        }
+
+        checkRep();
+        return bSuccess;
     }
 }

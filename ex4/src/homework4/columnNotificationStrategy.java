d23a0155a6d1e13e590285488b698c3f2116954f
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-19 17:28:41 +0200

    new files

--- ex4/src/homework4/columnNotificationStrategy.java
+++ ex4/src/homework4/columnNotificationStrategy.java
@@ -3,7 +3,7 @@ package homework4;
 import java.awt.*;
 import java.util.List;
 
-public class decreasingNotificationStrategy implements NotificationStrategy {
+public class columnNotificationStrategy implements NotificationStrategy {
     @Override
     public void notifyAll(List<Panel> panels, Color color) {
 

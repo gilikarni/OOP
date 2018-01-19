bbf306b1d4758ca6bfc31e5d1e5901af164ffc90
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-19 18:41:43 +0200

    new files

--- ex4/src/homework4/RandomNotificationStrategy.java
+++ ex4/src/homework4/RandomNotificationStrategy.java
@@ -1,11 +1,10 @@
 package homework4;
 
-import java.awt.*;
 import java.util.List;
 
 public class RandomNotificationStrategy implements NotificationStrategy {
     @Override
-    public void notifyAll(List<Panel> panels, Color color) {
+    public List<Integer> getOrder() {
 
     }
 }

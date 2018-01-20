92044d401686b00deda57109213cf52571814ca8
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-20 10:46:20 +0200

    create updateNextPanel

--- ex4/src/homework4/ColorGenerator.java
+++ ex4/src/homework4/ColorGenerator.java
@@ -3,7 +3,6 @@ package homework4;
 import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,10 +13,10 @@ public class ColorGenerator implements Observable, ActionListener {
     static private ColorGenerator colorGenerator = new ColorGenerator();
     private ArrayList<Panel> listeners = new ArrayList<>();
     NotificationStrategy notificationStrategy = new IncreasingNotificationStrategy();
-    final static int numberOfPanels = 25;
-    final static int time_between_new_color = 2000;
-    final static int time_between_updates = 40;
-    Timer timer = new Timer(time_between_new_color, this);
+    final static int NUMBER_OF_PANELS = 25;
+    private ArrayList<Integer> order = null;
+    private int indexInOrder = 0;
+    Color color = null;
 
     /** Abstraction Function:
      * ColorGenerator c is a singleton that is responsible for creating a new color every two seconds and update all the
@@ -37,7 +36,7 @@ public class ColorGenerator implements Observable, ActionListener {
      */
     private void checkRep() {
         assert colorGenerator != null : "colorGenerator is null";
-        assert listeners.size() > numberOfPanels : "There are " + listeners.size() + "listeners";
+        assert listeners.size() > NUMBER_OF_PANELS : "There are " + listeners.size() + "listeners";
     }
 
     /**
@@ -80,22 +79,23 @@ public class ColorGenerator implements Observable, ActionListener {
      * @effects generates a new random color and update all the panels about it. If all the panels wasn't added yet the
      * function will do nothing.
      */
-    public void generateNewColor() throws InterruptedException {
-        if (listeners.size() < numberOfPanels) { /* The panels wasn't added yet */
+    public void updateNextPanel() throws InterruptedException {
+        if (listeners.size() < NUMBER_OF_PANELS) { /* The panels wasn't added yet */
             return;
         }
 
-        ArrayList<Integer> order = (ArrayList<Integer>) notificationStrategy.getOrder();
-        Random randomColorGenerator = new Random();
-        float r = randomColorGenerator.nextFloat();
-        float g = randomColorGenerator.nextFloat();
-        float b = randomColorGenerator.nextFloat();
-        Color color = new Color(r, g, b);
-
-        for (Integer panel : order) {
-            listeners.get(panel).update(this, color);
-            Thread.sleep(time_between_updates);
+        if (indexInOrder == 0) {
+            order = (ArrayList<Integer>) notificationStrategy.getOrder();
+            Random randomColorGenerator = new Random();
+            float r = randomColorGenerator.nextFloat();
+            float g = randomColorGenerator.nextFloat();
+            float b = randomColorGenerator.nextFloat();
+            color = new Color(r, g, b);
         }
+
+        listeners.get(order.get(indexInOrder)).update(this, color);
+        indexInOrder++;
+        indexInOrder %= NUMBER_OF_PANELS;
         checkRep();
     }
 

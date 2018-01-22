0b55145a6c1216f64f4941bd07d6ab9b03a6fdb1
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-22 17:51:57 +0200

    Added documentation to NotificationStrategy.java

--- ex4/src/homework4/NotificationStrategy.java
+++ ex4/src/homework4/NotificationStrategy.java
@@ -2,6 +2,12 @@ package homework4;
 
 import java.util.List;
 
+/**
+ * Creates an array of indices in the range [0,24]
+ */
 interface NotificationStrategy {
+    /**
+     * @effects creates a new index array from the range [0,24]
+     */
     abstract public List<Integer> getOrder();
 }

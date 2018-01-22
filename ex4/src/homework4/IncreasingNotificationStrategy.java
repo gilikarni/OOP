804c9cd4031649d5319cf368b57fbd041198a614
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-22 17:53:44 +0200

    Added documentation to IncreasingNotificationStrategy.java

--- ex4/src/homework4/IncreasingNotificationStrategy.java
+++ ex4/src/homework4/IncreasingNotificationStrategy.java
@@ -3,7 +3,13 @@ package homework4;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * IncreasingNotificationStrategy creates an array of indices in increasing order from the range [0,24]
+ */
 public class IncreasingNotificationStrategy implements NotificationStrategy {
+    /**
+     * @effects create an array with sorted indices from the range [0,24]
+     */
     @Override
     public List<Integer> getOrder() {
         ArrayList<Integer> array = new ArrayList<>();

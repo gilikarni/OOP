85da3d66d4a89a9ded32f603670d74a5796d9430
gilikarni <gilikarni@campus.technion.ac.il>
2018-01-22 17:56:46 +0200

    Added documentation to IncreasingNotificationStrategy.java

--- ex4/src/homework4/TwoTimesNotificationStrategy.java
+++ ex4/src/homework4/TwoTimesNotificationStrategy.java
@@ -3,7 +3,14 @@ package homework4;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * TwoTimesNotificationStrategy creates an array of indices in the range [0,24]. The array contains a concatenation of
+ * the evan indices sorted in increasing order and than the odd indices sorted in increasing order.
+ */
 public class TwoTimesNotificationStrategy implements NotificationStrategy {
+    /**
+     * @effects create an array of indices as explained in the class description.
+     */
     @Override
     public List<Integer> getOrder() {
         ArrayList<Integer> array = new ArrayList<>();
